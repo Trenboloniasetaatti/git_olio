36c7343bd86911cce0ddf257d4f3e607f982955a
Trenboloniasetaatti <cuyar510@gmail.com>
2024-04-19 10:32:55 +0300

    Ruoka

---
+++ Ruoka-annos_Generaattori/Ruoka-annos_Generaattori/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public enum PääraakaAine
+{
+    Nauta,
+    Kana,
+    Kasvikset
+}
+
+public enum Lisuke
+{
+    Peruna,
+    Riisi,
+    Pasta
+}
+
+public enum Kastike
+{
+    Curry,
+    Hapanimelä,
+    Pippuri,
+    Chili
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Valitse pääraaka-aine (0: Nauta, 1: Kana, 2: Kasvikset): ");
+        PääraakaAine valittuPääraakaAine = (PääraakaAine)int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Valitse lisuke (0: Peruna, 1: Riisi, 2: Pasta): ");
+        Lisuke valittuLisuke = (Lisuke)int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Valitse kastike (0: Curry, 1: Hapanimelä, 2: Pippuri, 3: Chili): ");
+        Kastike valittuKastike = (Kastike)int.Parse(Console.ReadLine());
+
+        var ruokaAnnos = (valittuPääraakaAine, valittuLisuke, valittuKastike);
+
+        Console.WriteLine($"Ruoka-annos: {ruokaAnnos}");
+    }
+}
+
